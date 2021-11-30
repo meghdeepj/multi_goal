1dbc4af11bc7b91dbba72fecd51eec20ea206c10
XK20 <xikaid@andrew.cmu.edu>
2021-11-30 12:53:37 -0500

    modified to arbitrary input number

--- task_planner.cpp
+++ task_planner.cpp
@@ -14,22 +14,26 @@ using namespace std;
 int main()
 
 {   
-    vector<int> start_point={1,1};
+    pair<int,int> start_point(1,1);
     queue<pair<int,int>> goal_pose;
     vector<pair<int,int>> results;
+    goal_pose.push (make_pair(1, 5));
+    goal_pose.push (make_pair(1, 3));
     goal_pose.push (make_pair(1, 2));
-    goal_pose.push (make_pair(3, 1));
-    goal_pose.push (make_pair(3, 3));
+    goal_pose.push (make_pair(3, 5));
+    goal_pose.push (make_pair(5, 5));
+
 
     
 
     Taskplanner task(start_point,goal_pose);
-    task.euclidean();
+    task.queuepreprocess();
     results=task.computeorder();
     for(int i=0;i<results.size();i++)
     {
         cout<<results[i].first<<" "<<results[i].second<<endl;
     }
+    //*/
     //cout<<"the order of the point coordiante is "<<  <<endl;
     return 0;
 }
--- task_planner.hpp
+++ task_planner.hpp
@@ -2,109 +2,65 @@
 using namespace std;
 class Taskplanner
 {   
-    vector<int> start_point;
+    pair<int,int> start_point;
     queue<pair<int,int>> goal_pose;
 
     public:
-        double distance_Str_G1;
-        double distance_Str_G2;
-        double distance_Str_G3;
-        double G1_G2;
-        double G1_G3;
-        double G2_G3;
  
         vector<pair<int,int>> goals;
         vector<pair<int,int>> results;
         
 
-        Taskplanner(vector<int> start_point, queue<pair<int,int>> goal_pose)
+        Taskplanner(pair<int,int> start_point, queue<pair<int,int>> goal_pose)
         {
             this->start_point = start_point;
             this->goal_pose = goal_pose;
         }
 
-        double calc1 (vector<int> in1,pair<int,int> in2)
-        {
-            return sqrt(pow((in1[0]-in2.first),2)+pow((in1[1]-in2.second),2));
-        }
-
-        double calc2 (pair<int,int> in1,pair<int,int> in2)
+        double calc1 (pair<int,int> in1,pair<int,int> in2)
         {
             return sqrt(pow((in1.first-in2.first),2)+pow((in1.second-in2.second),2));
         }
 
-
         //input set of point coordinate goal_coor(vector of pairs) and start_point(pair)...
-        void euclidean()//pair start,quene<pair<int,int>> goal_pose);
+        void queuepreprocess()//pair start,quene<pair<int,int>> goal_pose);
         {   
             int temp = goal_pose.size();
             for (int i=0;i<temp;i++)
             {
-                goals.push_back(goal_pose.front());
-                cout<<goal_pose.front().first<<goal_pose.front().second<<endl;
+                goals.push_back(goal_pose.front());  
                 goal_pose.pop();            
             }
-            distance_Str_G1 = calc1(start_point,goals[0]); 
-            distance_Str_G2 = calc1(start_point,goals[1]);
-            distance_Str_G3 = calc1(start_point,goals[2]);
-
-            G1_G2 = calc2(goals[0],goals[1]); 
-            G1_G3 = calc2(goals[0],goals[2]);
-            G2_G3 = calc2(goals[1],goals[2]);
-
+            cout<<"preprocess complete"<<endl;
         }
 
+        
         vector<pair<int,int>> computeorder()
-        {
-            if (distance_Str_G1<=distance_Str_G2 && distance_Str_G1<=distance_Str_G3)
+        {   
+            vector<pair<int,int>> sequence;
+            int loop = goals.size();
+            pair<int,int> prev_vec = start_point;
+            for(int i=0;i<loop;i++)
             {
-                if(G1_G2<=G1_G3)
+                int min_val=numeric_limits<int>::max();
+                pair<int,int> min_vec;
+                int min_index;
+                //cout<<"debug"<<endl;
+                for(int j=0;j<goals.size();j++)
                 {
-                    results.push_back(goals[0]);
-                    results.push_back(goals[1]);
-                    results.push_back(goals[2]);
+                    if(calc1(prev_vec,goals[j])<=min_val)
+                    {
+                        min_val=calc1(prev_vec,goals[j]);
+                        min_vec=goals[j];
+                        min_index=j;
+                    }
                 }
-                else
-                {
-                    results.push_back(goals[0]);
-                    results.push_back(goals[2]);
-                    results.push_back(goals[1]);
-                }
+                sequence.push_back(min_vec);
+                goals.erase(goals.begin()+min_index);
+                prev_vec = min_vec;
             }
-            else if (distance_Str_G2<=distance_Str_G1 && distance_Str_G2<=distance_Str_G3)
-            {
-                if(G1_G2<=G2_G3)
-                {
-                    results.push_back(goals[1]);
-                    results.push_back(goals[0]);
-                    results.push_back(goals[2]);
-                }
-                else
-                {
-                    results.push_back(goals[1]);
-                    results.push_back(goals[2]);
-                    results.push_back(goals[0]);
-                }
-            }
-            else
-            {
-                if(G1_G3<=G2_G3)
-                {
-                    results.push_back(goals[2]);
-                    results.push_back(goals[0]);
-                    results.push_back(goals[1]);
-                }
-                else
-                {
-                    results.push_back(goals[2]);
-                    results.push_back(goals[1]);
-                    results.push_back(goals[0]);
-                }
-            }
-
-            cout<<"goal sequence generated "<< endl;
-            
-            return results;
+            return sequence;
         }
+        
         
 };
